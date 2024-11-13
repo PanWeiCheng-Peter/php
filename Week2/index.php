cab12b004d519cc3edf2ee05eae2b7b30be9a688
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-11-13 14:51:27 +0800

    index-css-margin0px,<p></p>

--- Week2/index.php
+++ Week2/index.php
@@ -1,16 +1,40 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <style>
-        body {background-color: greenyellow;}
-    </style>    
+        body {
+            background-color: greenyellow;
+        }
+
+        .redtext {
+            color: red;
+            margin-bottom: 0px;
+            margin-top: 0px;
+        }
+
+        .bluetext {
+            color: blue;
+            margin-bottom: 0px;
+            margin-top: 0px;
+        }
+        .greytext {
+            color: grey;
+            margin-bottom: 0px;
+            margin-top: 0px;
+        }
+    </style>
 </head>
+
 <body>
 
-<?php
-echo "My first PHP script!";
-echo date ("d M Y ");
-?>
+    <?php
+    date_default_timezone_set('Asia/Kuala_Lumpur');
+    echo "<p class=redtext> My first PHP script!</p>";
+    echo "<p class=bluetext>" . date(" d M Y ") . "</p>";
+    echo "<p class=greytext>" . date(" h:i:s") . "</p>";
+    ?>
 
 </body>
+
 </html>
