12cc7dda15e25e4bb0bd24f6369a24951d82f18f
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-11-13 15:21:46 +0800

    text again

--- Week2/random_num.php
+++ Week2/random_num.php
@@ -37,10 +37,10 @@
     <?php
     $num1=rand(100,200);
     $num2=rand(100,200);
-    echo "<p class=text1>"."The first number in line 1:".($num1)."</p>";
-    echo "<p class=text2>"."The second number in line 2:".($num2)."</p>";
-    echo "<p class=text3>"."The sum of both variables in line 3:".($num1+$num2)."</p>";
-    echo "<p class=text4>"."The multiplication of both variables in line 4:".($num1*$num2)."</p>";
+    echo "<p class=text1>"."The first number is ".($num1)."</p>";
+    echo "<p class=text2>"."The second number is ".($num2)."</p>";
+    echo "<p class=text3>"."The sum of both variables is ".($num1+$num2)."</p>";
+    echo "<p class=text4>"."The multiplication of both variables is ".($num1*$num2)."</p>";
     ?>
 
 </body>
