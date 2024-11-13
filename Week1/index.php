bc7eb5917f8e17ff8f4b28560575215e527f2daa
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-11-13 14:02:38 +0800

    Date

---
+++ Week1/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My first php</title>
+    </head>
+<body>
+
+    <?php
+    echo "Pan Wei Cheng";
+    ?>
+</body>
+</html>
---
+++ Week2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {background-color: greenyellow;}
+    </style>    
+</head>
+<body>
+
+<?php
+echo "My first PHP script!";
+echo date ("d M Y ");
+?>
+
+</body>
+</html>
