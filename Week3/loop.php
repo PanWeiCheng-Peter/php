c3773235f03cc9316ea28622394fbe39487c6142
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-11-20 14:56:54 +0800

    EXE6 loop

---
+++ Week3/loop.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    for ($n = 0; $n <= 100; $n++) {
+        if ($n % 2 == 0){
+            echo "<p>"."The number is: $n </p>";
+        }
+        
+    }
+    ?>
+</body>
+</html>
