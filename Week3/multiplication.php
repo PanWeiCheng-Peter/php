6cf740b6c5929851dded2d1fcb645677fc2708fa
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-11-20 15:19:02 +0800

    exe 8 done

---
+++ Week3/multiplication.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php
+        $num = 5;
+        for ($i = 1; $i <= 10; $i++) {
+            echo ("<p>$num"
+                . " X " . "$i" . " = "
+                . $num * $i . "</p> 
+        ");
+        }
+    ?>
+
+</head>
+
+</html>
