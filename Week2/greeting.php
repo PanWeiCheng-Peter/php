563b188f96da7211bb0169e456f2301f6df14544
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-11-20 13:46:20 +0800

    print echo num and time

--- Week2/greeting.php
+++ Week2/greeting.php
@@ -5,17 +5,18 @@
         $time = rand(0,23);
 
         if ($time > 4 && $time < 12) {
-            echo "Good Morning!" . ($time) . "</p>";
+            echo "Good Morning!";
         }
         else if ($time > 11 && $time < 18) {
-            echo "Good Afternoon!" . ($time) . "</p>";
+            echo "Good Afternoon!";
         }
         else if ($time > 17 && $time < 22) {
-            echo "Good evening!" . ($time) . "</p>";
+            echo "Good evening!";
         }
         else {
-            echo "Good night!" . ($time) . "</p>";
+            echo "Good night!";
         }
+        echo $time;
         ?>
     </body>
 
--- Week2/odd_even.php
+++ Week2/odd_even.php
@@ -14,11 +14,12 @@
     <?php
     $num1 = rand(1, 100);
     if ($num1 % 2 == 0) {
-        echo "<p class=text1>" . "Even" . ($num1) . "</p>";
+        echo "<p class=text1>" . "Even";
     } 
     else {
-        echo "<p class=text1>" . "Odd" . ($num1) . "</p>";
+        echo "<p class=text1>" . "Odd";
     }
+    echo $num1;
     ?>
     
 </body>
