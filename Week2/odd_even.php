874e050d3216c4bef4db1f963421656b04849b81
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-11-20 13:26:56 +0800

    odd even num if else

---
+++ Week2/odd_even.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<style>
+    body {
+        background-color: grey;
+    }
+    .text1 {
+        color: green;
+        margin-bottom: 0px;
+        margin-top: 0px;
+    }
+</style>
+<body>
+    <?php
+    $num1 = rand(1, 100);
+    if ($num1 % 2 == 0) {
+        echo "<p class=text1>" . "Odd" . ($num1) . "</p>";
+    } 
+    else {
+        echo "<p class=text1>" . "Even" . ($num1) . "</p>";
+    }
+    ?>
+    
+</body>
+</html>
