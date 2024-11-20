e4168e5412b8ba1dcbbd4389ece2f34eb895193e
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-11-20 13:39:20 +0800

    exercise 4-1

---
+++ Week2/greeting.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        $time = rand(0,23);
+
+        if ($time > 5 && $time < 12) {
+            echo "Good Morning!" . ($time) . "</p>";
+        }
+        else if ($time > 11 && $time < 18) {
+            echo "Good Afternoon!" . ($time) . "</p>";
+        }
+        else if ($time > 17 && $time < 22) {
+            echo "Good evening!" . ($time) . "</p>";
+        }
+        else {
+            echo "Good night!" . ($time) . "</p>";
+        }
+        ?>
+    </body>
+
+</html>
