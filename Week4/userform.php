7123259236c40c3cee5c23fdcf8abebcdf96825b
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-11-27 14:34:35 +0800

    setting margin top and bottom

--- Week4/userform.php
+++ Week4/userform.php
@@ -1,24 +1,30 @@
 <!DOCTYPE HTML>
 <html>
+
 <head>
     <style>
         .redtext {
             color: red;
+            margin-bottom: 0px;
+            margin-top: 0px;
         }
     </style>
 </head>
 <body>
 
     Welcome <?php echo $_POST["name"]; ?><br>
-    Your Email: <?php echo $_POST["email"]; ?><br>
-    Your Age: <?php echo $_POST["age"]; ?><br>
     <?php
     if ($_POST["name"] == "") {
         echo "<p class=redtext>" . "Please enter your name." . "</p>";
-    }
+    } ?>
+    Your Email: <?php echo $_POST["email"]; ?><br>
+    <?php
     if ($_POST["email"] == "") {
         echo "<p class=redtext>" . "Please enter your email." . "</p>";
     }
+    ?>
+    Your Age: <?php echo $_POST["age"]; ?><br>
+    <?php
     if ($_POST["age"] == "") {
         echo "<p class=redtext>" . "Please enter your age." . "</p>";
     }
