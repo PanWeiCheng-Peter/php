0d2aa803bd06bdbdf1e1c1ec8aa6780a97f17e4d
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-11-27 15:20:37 +0800

    data self

--- Week4/data.php
+++ Week4/data.php
@@ -1,12 +1,36 @@
+<?php
+if (isset($_POST["name"])) {
+    echo "Welcome " . $_POST["name"] . "<br>";
+    if ($_POST["name"] == "") {
+        echo "<p class=redtext>" . "Please enter your name." . "</p>";
+    }
+    echo " Your name: " . $_POST["email"] . "<br>";
+    if ($_POST["email"] == "") {
+        echo "<p class=redtext>" . "Please enter your email." . "</p>";
+    }
+}
+?>
 <!DOCTYPE HTML>
-<html>  
-  <body>
+<html>
+
+<head>
+    <style>
+        .redtext {
+            color: red;
+            margin-bottom: 0px;
+            margin-top: 0px;
+        }
+    </style>
+</head>
+
+<body>
 
     <form action="welcome.php" method="post">
-Name: <input type="text"name="name"><br>
-E-mail: <input type="text"name="email"><br>
-<input type="submit">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
     </form>
 
-  </body>
+</body>
+
 </html>
