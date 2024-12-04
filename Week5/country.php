0a7ac1a56b699981d52c827fa947e80bb4fc9103
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-12-04 13:58:16 +0800

    country.php done sucessfully

--- Week5/country.php
+++ Week5/country.php
@@ -1,13 +1,25 @@
 <?php
-if (isset($_GET["country"])) {
+if (isset($_GET["country"])){
     echo "Your country is " . $_GET["country"] . "<br>";
+}
+if (isset($_GET["day"])) {
+    echo "" . $_GET["day"] . "<br>";
+}
+if (isset($_GET["month"])) {
+    echo "" . $_GET["month"] . "<br>";
+}
+if (isset($_GET["year"])) {
+    echo "" . $_GET["year"] . "<br>";
+}
+if (isset($_GET["gender"])) {
+    echo "" . $_GET["gender"] . "<br>";
 }
 ?>
 <!DOCTYPE html>
 <html>
 
 <body>
-    <h1>Select your Country</h1>
+    <h1>Country</h1>
     <p>Please select your country.</p>
     <form method="GET" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
         <label for="country">Choose your country</label>
@@ -24,7 +36,35 @@ if (isset($_GET["country"])) {
             <option value="Korea">Korea</option>
             <option value="India">India</option>
         </select>
-        <input type="submit">
+        <label for="day">Day</label>
+        <select name="day" id="day">
+            <?php
+            for ($i = 1; $i <= 31; $i++) {
+                echo "<option value=" . $i . ">" . $i . "</option>";
+            }
+            ?>
+        </select>
+        <label for="month">Month</label>
+        <select name="month" id="month">
+            <?php
+            for ($i = 1; $i <= 12; $i++) {
+                echo "<option value=" . $i . ">" . $i . "</option>";
+            }
+            ?>
+        </select>
+        <label for="year">Year</label>
+        <select name="year" id="year">
+            <?php
+            for ($i = 2000; $i <= 2024; $i++) {
+                echo "<option value=" . $i . ">" . $i . "</option>";
+            }
+            ?>
+        </select>
+        <label for="gender">Gender</label>
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "female") echo "checked"; ?> value="female">Female
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "male") echo "checked"; ?> value="male">Male
+        <br><br>
+        <input type="submit" name="submit" value="submit">
     </form>
 </body>
 
