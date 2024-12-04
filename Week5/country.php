a2082aceed4930349fbd192aeb08166de63f6d31
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-12-04 13:24:45 +0800

    country php

--- Week5/country.php
+++ Week5/country.php
@@ -1,17 +1,18 @@
 <?php
 if (isset($_GET["country"])) {
-    echo "Your country is" . $_GET["country"] . "<br>";
+    echo "Your country is " . $_GET["country"] . "<br>";
 }
 ?>
 <!DOCTYPE html>
 <html>
+
 <body>
     <h1>Select your Country</h1>
-    <p>Please select your country in down list.</p>
-    <form method="GET" action="/action_page.php">
+    <p>Please select your country.</p>
+    <form method="GET" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
         <label for="country">Choose your country</label>
         <select name="country" id="country">
-            <option value="" Selected>Country...</option>
+            <option value="" Selected>Select Your Country</option>
             <option value="Malaysia">Malaysia</option>
             <option value="Singapore">Singapore</option>
             <option value="Indonesia">Indonesia</option>
@@ -23,8 +24,8 @@ if (isset($_GET["country"])) {
             <option value="Korea">Korea</option>
             <option value="India">India</option>
         </select>
-        <br></br>
-        <input type="sumbit" value="Sumbit">
+        <input type="submit">
     </form>
 </body>
+
 </html>
