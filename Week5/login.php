32cf5d89754799b5ee2e3af49abec251a580e118
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-12-04 15:22:32 +0800

    login and session

--- Week5/login.php
+++ Week5/login.php
@@ -1,19 +1,26 @@
 <?php
-// Start the session
-session_start();
+$InputUsername = "admin";
+$InputPassword = "1234";
+if (isset($_POST["username"])) {
+    if (isset($_POST["password"])) {
+        if ($_POST["username"] == $InputUsername) {
+            if ($_POST["password"] == $InputPassword) {
+                echo "<p>" . "Welcome " . ($_POST["username"]). "</p>";
+            }else
+            echo "<p>"."Please login again.". "</p>";}
+        }
+    }       
 ?>
+
 <!DOCTYPE html>
 <html>
 
 <body>
-
-    <?php
-    // Set session variables
-    $_SESSION["Username"] = "admin";
-    $_SESSION["password"] = "1234";
-    echo "Session variables are set.";
-    ?>
-
+    <p>Please enter your username and password.</p>
+    <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+        Username: <input type="text" name="username"><br>
+        Password: <input type="text" name="password"><br>
+        <input type="submit" name="submit" value="submit">
 </body>
 
 </html>
---
+++ Week5/session.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    $_SESSION["username"] = "admin";
+    $_SESSION["password"] = "1234";
+    echo "Session variables are set.";
+    ?>
+
+</body>
+
+</html>
