77bfd53c21ef18f88ebb3b0b8f1967d459d8e3b3
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-12-04 15:24:55 +0800

    1

--- Week5/session.php
+++ Week5/session.php
@@ -1,17 +1,14 @@
 <?php
 session_start();
 ?>
+
 <!DOCTYPE html>
 <html>
-
 <body>
-
     <?php
     $_SESSION["username"] = "admin";
     $_SESSION["password"] = "1234";
     echo "Session variables are set.";
     ?>
-
 </body>
-
 </html>
