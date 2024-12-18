44e0d23599e09299d5ef80f56234a0b91a88dac7
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2024-12-18 12:58:24 +0800

    week 6

--- Week5/session.php
+++ Week5/session.php
@@ -4,11 +4,15 @@ session_start();
 
 <!DOCTYPE html>
 <html>
+
 <body>
     <?php
     $_SESSION["username"] = "admin";
     $_SESSION["password"] = "1234";
+
     echo "Session variables are set.";
+    echo "<a href='counter.php'>Login</a>";
     ?>
 </body>
+
 </html>
---
+++ shop/product_listing.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Read Products</h1>
+        </div>
+
+        <!-- PHP code to read records will be here -->
+
+    </div> <!-- end .container -->
+
+    <!-- confirm delete record will be here -->
+
+</body>
+
+</html>
