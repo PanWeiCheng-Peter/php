ca59d38b0c889cd7da9ae513ec6bd0aabd01ace1
panweicheng1007@e.neivce.edu.my <panweicheng1007@e.neivce.edu.my>
2025-01-17 22:48:04 +0800

    update small debug

--- shop/product_update.php
+++ shop/product_update.php
@@ -61,19 +61,27 @@
                 // in this case, it seemed like we have so many fields to pass and
                 // it is better to label them and not use question marks
                 $query = "UPDATE products
-                  SET name=:name, description=:description,
-                  price=:price WHERE id = :id";
+                        SET name=:name, description=:description,
+                        price=:price, promotion_price=:promotion_price, 
+                        manufacture_date=:manufacture_date, expired_date=:expired_date 
+                        WHERE id = :id";
                 // prepare query for excecution
                 $stmt = $con->prepare($query);
                 // posted values
                 $name = htmlspecialchars(strip_tags($_POST['name']));
                 $description = htmlspecialchars(strip_tags($_POST['description']));
                 $price = htmlspecialchars(strip_tags($_POST['price']));
+                $promotion_price = htmlspecialchars(strip_tags($_POST['promotion_price']));
+                $manufacture_date = htmlspecialchars(strip_tags($_POST['manufacture_date']));
+                $expired_date = htmlspecialchars(strip_tags($_POST['expired_date']));
                 // bind the parameters
                 $stmt->bindParam(':name', $name);
                 $stmt->bindParam(':description', $description);
                 $stmt->bindParam(':price', $price);
                 $stmt->bindParam(':promotion_price', $promotion_price);
+                $stmt->bindParam(':manufacture_date', $manufacture_date);
+                $stmt->bindParam(':expired_date', $expired_date);
+
                 $stmt->bindParam(':id', $id);
                 // Execute the query
                 if ($stmt->execute()) {
